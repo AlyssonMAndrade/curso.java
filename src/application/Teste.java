9a1a41db73dcd1d56815503a3071af4da75f91ca
AlyssonMAndrade <marquesandrade.alysson@gmail.com>
2020-08-03 16:24:06 -0300

    Segunda Versao

--- src/application/Teste.java
+++ src/application/Teste.java
@@ -6,6 +6,17 @@ public class Teste {
 		// Primeiro Projeto no Repositorio GIT
 		
 		System.out.println("Bom dia, Galera!");
+		
+		/*
+		 * Passos no Github
+		 * (1) Verificar arquivos no github: $ git status
+		 * (2) Colocar arquivos no stage: $ git add .
+		 * (3) Comitar : $ git commit -m "Mensagem Explicativa"
+		 * (4) Salva no Github: git push
+		 * 
+		 */
+		
+		System.out.println("Comentarios Acrescentados para explicar Github.");
 
 	}
 
